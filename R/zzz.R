838d45ff8141a342a4567ff464ac75ea4306a40d
al-obrien <allen.g.obrien@gmail.com>
2023-10-23 17:29:21 -0600

    Add global environ variable for possible future functionality
    Might be useful if using objects and want to enforce temp folder location across them for downloads as opposed to within different objects. Will also attempt to clean up the folder if removing the package.

---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+.onLoad <- function(libname, pkgname) {
+  Sys.setenv(ROSV_CACHE_GLOBAL = file.path(tempdir(), 'rosv'))
+}
+
+.onUnload <- function() {
+  if(dir.exists(Sys.getenv("ROSV_CACHE_GLOBAL"))) try(unlink(Sys.getenv("ROSV_CACHE_GLOBAL"), TRUE, TRUE, TRUE))
+  Sys.unsetenv(ROSV_CACHE_GLOBAL)
+}
