9dbe103507d6eec50eb93164b9dbdd41a417d549
al-obrien <allen.g.obrien@gmail.com>
2023-10-28 00:53:12 -0600

    Add basic test
    To add more useful ones later...

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,3 +1,8 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+without_internet({
+  test_that("Can query PyPI...", {
+    query_1 <- RosvQuery1$new()
+    expect_POST(query_1$run(name = 'dask', ecosystem = 'PyPI'),
+                'https://api.osv.dev/v1/query',
+                '{"commit":null,"version":null,"package":{"name":"dask","ecosystem":"PyPI","purl":null},"page_token":null}')
+  })
 })
