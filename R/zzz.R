cb2c12e3de795d914bfff1d75942c80722487a5c
al-obrien <allen.g.obrien@gmail.com>
2023-10-28 12:42:48 -0600

    Add dummy function to ensure httr2 is picked up
    Avoids CRAN note when all httr2 references are inside R6 methods

--- R/zzz.R
+++ R/zzz.R
@@ -7,3 +7,8 @@
   if(dir.exists(Sys.getenv("ROSV_CACHE_GLOBAL"))) try(unlink(Sys.getenv("ROSV_CACHE_GLOBAL"), TRUE, TRUE, TRUE))
   Sys.unsetenv("ROSV_CACHE_GLOBAL")
 }
+
+# Function to enforce httr2 use to remove CRAN note (most was nested in R6 methods)
+enforce_httr2_use <- function() {
+  httr2::request('dummyURL')
+}
