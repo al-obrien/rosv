2faf06423c6c6925fcaa38efaf740564f834ca9a
al-obrien <allen.g.obrien@gmail.com>
2023-11-01 10:30:45 -0600

    More tests on rosv type expectations

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -7,3 +7,19 @@ test_that('Correct ecosystems can be checked...', {
   expect_equal(check_ecosystem(c('PyPI', 'CRAN')), c('PyPI', 'CRAN'))
   expect_error(check_ecosystem(c('pypi')))
 })
+
+test_that('Ensure rosv type check is selective...', {
+  expect_true(is_rosv(RosvQuery1$new(name = 'pandas', ecosystem = 'PyPI')))
+  expect_true(is_rosv(RosvQueryBatch$new(name = 'pandas', ecosystem = 'PyPI')))
+  expect_true(is_rosv(RosvVulns$new('RSEC-2023-6')))
+
+  expect_true(validate_rosv(RosvQuery1$new(name = 'pandas', ecosystem = 'PyPI')))
+  expect_true(validate_rosv(RosvQueryBatch$new(name = 'pandas', ecosystem = 'PyPI')))
+  expect_true(validate_rosv(RosvVulns$new('RSEC-2023-6')))
+
+  expect_false(is_rosv(c(1,2,3)))
+  expect_false(is_rosv(data.frame(test = c(1,2,3))))
+
+  expect_error(validate_rosv(c(1,2,3)))
+  expect_error(validate_rosv(data.frame(test = c(1,2,3))))
+})
