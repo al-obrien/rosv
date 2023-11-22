e58fd2a29391ad78c4c87a21178c6a4dd68e642a
al-obrien <allen.g.obrien@gmail.com>
2023-11-22 14:52:22 -0700

    Basic tests added for counting vulns

--- tests/testthat/test-query.R
+++ tests/testthat/test-query.R
@@ -4,3 +4,12 @@ with_mock_dir('is_pkg_vul', {
     expect_equal(is_pkg_vulnerable(rep('readxl', 2), rep('CRAN', 2), version = c('1.4.1', '2.0.0')), c(readxl = TRUE, readxl = FALSE))
     })
 })
+
+with_mock_dir('osv_count_vul', {
+  test_that("Returns named vectors with correct vuln counts", {
+    expect_equal(osv_count_vulns(c( 'data.table'), c( 'CRAN')), c(data.table = 0))
+    expect_equal(osv_count_vulns(c('dask', 'data.table'), c('PyPI', 'CRAN')), c(dask = 1, data.table = 0))
+    expect_equal(osv_count_vulns(rep('dask',3), rep('PyPI',3), version = c('2.8.0', '9999', '2021.8.0')), c(dask = 1, dask = 0, dask = 1))
+    expect_equal(osv_count_vulns(rep('readxl', 2), rep('CRAN', 2), version = c('1.4.1', '2.0.0')), c(readxl = 1, readxl = 0))
+  })
+})
